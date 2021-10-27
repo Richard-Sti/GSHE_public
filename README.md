efe36263ca52e46ca6be0751c9709bb1dff071ce
Richard Stiskalek <richard.stiskalek@protonmail.com>
2021-10-27 16:35:25 +0200

    add cartesian to spherical

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Numerical gravitational spin Hall equations
+
+
+## TODO:
+- [ ] Add least squares to determine at which point the geodesic is a straight line
+- [ ] Calculate how close that straight line passes to a target location (the impact parameter)
+- [ ] Add how tau behaves along the straight line
+- [ ] Add some solver that minimises the impact parameter
---
+++ gr_spin_hall/utils.py
@@ -0,0 +1,47 @@
+# MIT License
+#
+# Copyright (c) 2021 Richard Stiskalek
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import numpy
+
+
+def spherical_to_cartesian(X):
+    """
+    The usual conversion of spherical to Cartesian coordinates.
+
+    Arguments
+    ---------
+    X: numpy.ndarray
+        Array of shape `(Npoints, 3)` where the second axis is `r`, `theta`,
+        and `phi` respectively.
+
+    Returns
+    -------
+    out: numpy.ndarray
+        Array of shape `(Npoints, 3)` where the second axis is `x`, `y`,
+        and `z` respectively.
+    """
+    stheta, ctheta = numpy.sin(X[:, 1]), numpy.cos(X[:, 1])
+    sphi, cphi = numpy.sin(X[:, 2]), numpy.cos(X[:, 2])
+    x = X[:, 0] * cphi * stheta
+    y = X[:, 0] * sphi * stheta
+    z = X[:, 0] * ctheta
+    return numpy.vstack([x, y, z]).T
