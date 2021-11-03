5bd4e92221b515f902c03169e55844e103750f14
Richard Stiskalek <richard.stiskalek@protonmail.com>
2021-11-03 15:04:24 +0100

    Add angular distance

--- gr_spin_hall/distance_measure.py
+++ gr_spin_hall/distance_measure.py
@@ -22,27 +22,37 @@
 
 import numpy
 # from .chain import Chain
-from .utils import spherical_to_cartesian
+# from .utils import spherical_to_cartesian
 
 
-def minkowski_geodesic_fit(integrator_linear, pars=['r', 'theta', 'phi']):
-    X = spherical_to_cartesian(integrator_linear.chain.get_data(pars))
+def angdist(point1, point2):
+    if not numpy.isclose(point1['r'], point2['r']):
+        raise ValueError("Add erro")
 
-    dX = X[1:, :] - X[0, :]
-    dX /= numpy.linalg.norm(dX, axis=1).reshape(-1, 1)
-
-    v = numpy.mean(dX, axis=0)
-    std = numpy.var(dX, axis=0).mean()**0.5
-    # TODO: Check that std is less than some boundary
-    return v, std
+    return numpy.arccos(
+        numpy.cos(point1['theta']) * numpy.cos(point2['theta'])
+        + numpy.sin(point1['theta']) * numpy.sin(point2['theta'])
+        * numpy.cos(point1['phi'] - point2['phi']))
 
 
-def cartesian_distance(integrator, integrator_linear, observer_coordinates,
-                       pars=['r', 'theta', 'phi']):
-    v, __ = minkowski_geodesic_fit(integrator_linear)
-    pars = ['r', 'theta', 'phi']
-    r_observer = spherical_to_cartesian([observer_coordinates[p]
-                                         for p in pars])
-    r0 = spherical_to_cartesian([integrator.current_coord(p) for p in pars])
-    return numpy.linalg.norm(numpy.dot(r_observer - r0, v) * v
-                             - r_observer + r0)
+# def minkowski_geodesic_fit(integrator_linear, pars=['r', 'theta', 'phi']):
+#     X = spherical_to_cartesian(integrator_linear.chain.get_data(pars))
+
+#     dX = X[1:, :] - X[0, :]
+#     dX /= numpy.linalg.norm(dX, axis=1).reshape(-1, 1)
+
+#     v = numpy.mean(dX, axis=0)
+#     std = numpy.var(dX, axis=0).mean()**0.5
+#     # TODO: Check that std is less than some boundary
+#     return v, std
+
+
+# def cartesian_distance(integrator, integrator_linear, observer_coordinates,
+#                        pars=['r', 'theta', 'phi']):
+#     v, __ = minkowski_geodesic_fit(integrator_linear)
+#     pars = ['r', 'theta', 'phi']
+#     r_observer = spherical_to_cartesian([observer_coordinates[p]
+#                                          for p in pars])
+#     r0 = spherical_to_cartesian([integrator.current_coord(p) for p in pars])
+#     return numpy.linalg.norm(numpy.dot(r_observer - r0, v) * v
+#                              - r_observer + r0)
