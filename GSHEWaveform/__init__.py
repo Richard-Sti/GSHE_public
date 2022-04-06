aee99237cab8d1a9e1b988ec4b20b3f2f8c07b3d
Richard Stiskalek <richard.stiskalek@protonmail.com>
2022-04-07 00:15:14 +0200

    Waveform calculations in Python (#11)
    * Documentation gshe_to_circular
    
    * More documentatoin
    
    * Add imports
    
    * add a blank line

--- GSHEWaveform/__init__.py
+++ GSHEWaveform/__init__.py
@@ -1,2 +1,3 @@
 from .utils import (epsilon, time_delay_analytical, linear_to_circular,
                     mixing, circular_to_linear)
+from .waveform import (gshe_to_circular, gshe_to_linear, fd_to_td_fiducialshift)
--- GSHEWaveform/waveform.py
+++ GSHEWaveform/waveform.py
@@ -1,41 +1,102 @@
 from copy import deepcopy
-
 import numpy
 from pycbc.waveform.utils import apply_fd_time_shift, fd_to_td
 
 from .utils import mixing, linear_to_circular, circular_to_linear
 
 
-
-
 def gshe_to_circular(hcirc_tilde, time_delay):
-
+    r"""
+    Apply GSHE correction to a null geodesic frequency-domain waveform in the
+    circular basis.
+    
+    Arguments
+    ---------
+    hcirc_tilde : :py:class:`pycbc.types.frequencyseries.FrequencySeries`
+        Frequency-domain circular basis waveform.
+    time_delay : :py:func:`time_delay(f)`
+        Observer time delay [s] of the particular circular polarisation whose
+        only argument is frequeyncy [Hz]. Must act on arrays.
+    
+    Returns
+    -------
+    hcirc_tilde_gshe : :py:class:`pycbc.types.frequencyseries.FrequencySeries`
+        Frequency-domain circular basis waveform with GSHE corrections applied.
+    """
     freqs = hcirc_tilde.sample_frequencies.data
-
     dt = time_delay(freqs)
-
+    # Deepcopy the waveform
     hcirc_tilde_gshe = deepcopy(hcirc_tilde)
-
+    # Multiply its data points by the mixing exp^{2pi i f dt}
     hcirc_tilde_gshe.data *= mixing(freqs, dt)
-
     return hcirc_tilde_gshe
 
 
 def gshe_to_linear(hptilde, hctilde, right_time_delay, left_time_delay):
-    
-    hrtilde, hltilde = linear_to_circular(hptilde, hctilde)
+    r"""
+    Apply GSHE corrections to null geodesic linearly polarised frequency-domain
+    waveforms.
 
+    Arguments
+    ---------
+    hptilde : :py:class:`pycbc.types.frequencyseries.FrequencySeries`
+        Frequency-domain plus polarisation waveform.
+    hctilde : :py:class:`pycbc.types.frequencyseries.FrequencySeries`
+        Frequency-domain cross polarisation waveform.
+    right_time_delay : :py:func:`time_delay(f)`
+        Observer time delay [s] of the right circular polarisation whose
+        only argument is frequeyncy [Hz]. Must act on arrays.
+    left_time_delay : :py:func:`time_delay(f)`
+        Observer time delay [s] of the left circular polarisation whose
+        only argument is frequeyncy [Hz]. Must act on arrays.
+
+    Returns
+    -------
+    hptilde_gshe : :py:class:`pycbc.types.frequencyseries.FrequencySeries`
+        Frequency-domain plus polarisation waveform with GSHE corrections.
+    hctilde_gshe : :py:class:`pycbc.types.frequencyseries.FrequencySeries`
+        Frequency-domain cross polarisation waveform with GSHE corrections.
+    """
+    hrtilde, hltilde = linear_to_circular(hptilde, hctilde)
+    # Apply correctoins in the circular basis
     hrtilde_gshe = gshe_to_circular(hrtilde, right_time_delay)
     hltilde_gshe = gshe_to_circular(hltilde, left_time_delay)
-
+    # Inverse transform back to the linear basis
     return circular_to_linear(hrtilde_gshe, hltilde_gshe)
 
     
-def fd_to_td_wshift(htilde, left_window=None, right_window=None, tshift=-1):
+def fd_to_td_fiducialshift(htilde, left_window=None, right_window=None,
+                           tshift=-1):
+    """
+    Inverse Fourier transform a frequency-domain waveform to the time domain
+    with a time shift to correct for :py:func:`pycbc.waveforms.utils.fd_to_td`
+    terminating the time series at :math:`t=0`.
+
+    Arguments
+    ---------
+    htilde : :py:class:`pycbc.types.frequencyseries.FrequencySeries`
+        Frequency-domain waveform.
+    left_window : tuple of float, optional
+        A tuple giving the start and end frequency of the FD taper to apply on
+        the left side. If None, no taper will be applied on the left.
+    right_window : tuple of float, optional
+        A tuple giving the start and end frequency of the FD taper to apply on
+        the right side. If None, no taper will be applied on the right.
+    tshift : float, optional
+        Fiducial time shift. Initially shifts the waveform backwards before
+        the IFT and then forward after. Must be negative, by default -1.
+
+    Returns
+    -------
+    htilde : :py:class:`pycbc.types.timeseries.TimeSeries`
+        Time-domain waveform.
+    """
+    if tshift > 0:
+        raise ValueError("`tshift` must be less than or equal to 0. "
+                         "Currently {}".format(tshift))
+    # Initial backwards shift
     htilde = apply_fd_time_shift(htilde, tshift)
     h = fd_to_td(htilde, left_window=left_window, right_window=right_window)
-    
+    # Shift back forwards
     h.start_time -= tshift
-
     return h
-
